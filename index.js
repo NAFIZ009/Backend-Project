4e9b0fb6651d63118e25c62379a68005c610916b
NAFIZ009 <jalalahmednafiz@gmail.com>
2023-01-27 17:04:45 +0600

    create-account

--- index.js
+++ index.js
@@ -1,19 +1,65 @@
+//implement
 const express = require('express');
+const cors = require('cors');
+const bcrypt = require('bcryptjs');
 const port =process.env.PORT || 5000;
 const app = express();
+
+//middleware
+app.use(cors());
+app.use(express.json());
+require('dotenv').config();
+
+//mongodb
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://jalal:<password>@cluster0.8juwqfy.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://jalal:${process.env.DB_PASSWORD}@cluster0.8juwqfy.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-
+//mongodb collections
+const userAccount=client.db("userAccount").collection("account");
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
 
+try{
 
+    //create user account
+    //client site should post a object with username and password in request body
+    app.post('/users',async (req, res) => {
+        const userInfo=req.body;
+        console.log(userInfo);
+        const {password}=userInfo;
+        //encrypting password
+        const salt =await bcrypt.genSaltSync(10);
+        const hashedPassword =await bcrypt.hashSync(password, salt);
+        if(!hashedPassword){
+            return res.status(400).json({
+                message: 'Account Creation Failed',
+            });
+        }
+        userInfo.password=hashedPassword;
+        const result = await userAccount.insertOne(userInfo);
+        if(!result.insertedId){
+            res.status(400).json({
+                message: 'Account Creation Failed',
+            });
+        }
+        res.status(201).json({
+            message: 'Account Created Successfully',
+        });
+    });
 
+    //testing 
+    // app.delete("/delete", async function(req, res) {
+    //     const result=await userAccount.deleteMany({});
+    //     res.send(result)
+    // })
+
+}catch(err){
+    console.log(err);
+}
 
 app.listen(port, () => {
   console.log('Server listening on port '+port);
